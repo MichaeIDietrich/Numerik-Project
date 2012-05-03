6d87c922649cf85c64489e77371b733371c6c7b9
Carsten <c2k10g@googlemail.com>
2012-05-03 10:15:36 +0200

    Testkommentar hinzugefügt Signed-off-by: Carsten <c2k10g@googlemail.com>

--- Numerik Project/src/numerik/calc/Matrix.java
+++ Numerik Project/src/numerik/calc/Matrix.java
@@ -36,7 +36,7 @@ public class Matrix {
         }
     }
     
-    // xgd
+    // Tescomment
     public Matrix(BigDecimal[][] values) {
         this.rows = values.length;
         this.cols = values[0].length;
