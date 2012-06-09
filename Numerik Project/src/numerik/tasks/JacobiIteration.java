8818f5f5f05891c37161e375ebebd51b397cd2dd
Carsten <c2k10g@googlemail.com>
2012-06-09 12:11:16 +0200

    JakobsFalcoIteration, mit Menü Fehlerkorrektur in data.txt Ergänzung Precision. Jetzt über Menü wählbar

--- Numerik Project/src/numerik/tasks/JacobiIteration.java
+++ Numerik Project/src/numerik/tasks/JacobiIteration.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import numerik.calc.*;
 import numerik.expression.Value;
+import numerik.tasks.Argument.ArgType;
 import numerik.ui.controls.ImageComponent;
 import numerik.ui.controls.TaskPane;
 import numerik.ui.controls.TaskScrollPane;
@@ -19,7 +20,14 @@ public class JacobiIteration implements Task
     @Override
     public void init(OutputFrame frame, TaskPane taskPane)
     {
+        MathLib.enableRound( true );
+        MathLib.setRoundingMode( MathLib.NORMAL ); 
+        MathLib.setNorm( MathLib.ZEILENSUMMENNORM );
+        
         this.taskPane = taskPane;
+        taskPane.createJToolBarByArguments(new Argument("Matrix:", ArgType.MATRIX, 100), 
+                                           new Argument("Vektor:", ArgType.VECTOR, 100),
+                                               Argument.PRECISION, Argument.RUN_BUTTON);
     }
     
     
@@ -30,38 +38,45 @@ public class JacobiIteration implements Task
         Recorder recorder = Recorder.getInstance();
         recorder.clear();
         
+        Matrix A = parameters[0].toMatrix();
+        Vector b = parameters[1].toVector();
+        MathLib.setPrecision( parameters[2].toDecimal().intValue() ); 
+        
         Matrix matrixInput;
         Vector vectorInput;
         
-        matrixInput = new Matrix(4, 4);
+        matrixInput = A.getDiagonalMatrix().getInverse().mult(BigDecimal.ONE.negate()).mult( (A.subtract( A.getDiagonalMatrix() )) );
+        vectorInput = A.getDiagonalMatrix().getInverse().mult(b);
         
-        matrixInput.set(0, 0, new BigDecimal("0"));
-        matrixInput.set(0, 1, new BigDecimal("0.1"));
-        matrixInput.set(0, 2, new BigDecimal("-0.2"));
-        matrixInput.set(0, 3, new BigDecimal("0"));
-        
-        matrixInput.set(1, 0, new BigDecimal("0.0909091"));
-        matrixInput.set(1, 1, new BigDecimal("0"));
-        matrixInput.set(1, 2, new BigDecimal("0.0909091"));
-        matrixInput.set(1, 3, new BigDecimal("-0.272727"));
-        
-        matrixInput.set(2, 0, new BigDecimal("-0.2"));
-        matrixInput.set(2, 1, new BigDecimal("0.1"));
-        matrixInput.set(2, 2, new BigDecimal("0"));
-        matrixInput.set(2, 3, new BigDecimal("0.1"));
-        
-        matrixInput.set(3, 0, new BigDecimal("0"));
-        matrixInput.set(3, 1, new BigDecimal("-0.375"));
-        matrixInput.set(3, 2, new BigDecimal("0.125"));
-        matrixInput.set(3, 3, new BigDecimal("0"));
-        
-        vectorInput = new Vector(4);
-        
-        vectorInput.set(0, new BigDecimal("0.6"));
-        vectorInput.set(1, new BigDecimal("2.27273"));
-        vectorInput.set(2, new BigDecimal("-1.1"));
-        vectorInput.set(3, new BigDecimal("1.875"));
-        
+//        matrixInput = new Matrix(4, 4);
+//        
+//        matrixInput.set(0, 0, new BigDecimal("0"));
+//        matrixInput.set(0, 1, new BigDecimal("0.1"));
+//        matrixInput.set(0, 2, new BigDecimal("-0.2"));
+//        matrixInput.set(0, 3, new BigDecimal("0"));
+//        
+//        matrixInput.set(1, 0, new BigDecimal("0.0909091"));
+//        matrixInput.set(1, 1, new BigDecimal("0"));
+//        matrixInput.set(1, 2, new BigDecimal("0.0909091"));
+//        matrixInput.set(1, 3, new BigDecimal("-0.272727"));
+//        
+//        matrixInput.set(2, 0, new BigDecimal("-0.2"));
+//        matrixInput.set(2, 1, new BigDecimal("0.1"));
+//        matrixInput.set(2, 2, new BigDecimal("0"));
+//        matrixInput.set(2, 3, new BigDecimal("0.1"));
+//        
+//        matrixInput.set(3, 0, new BigDecimal("0"));
+//        matrixInput.set(3, 1, new BigDecimal("-0.375"));
+//        matrixInput.set(3, 2, new BigDecimal("0.125"));
+//        matrixInput.set(3, 3, new BigDecimal("0"));
+//        
+//        vectorInput = new Vector(4);
+//        
+//        vectorInput.set(0, new BigDecimal("0.6"));
+//        vectorInput.set(1, new BigDecimal("2.27273"));
+//        vectorInput.set(2, new BigDecimal("-1.1"));
+//        vectorInput.set(3, new BigDecimal("1.875"));
+//        
         Vector startVectorX0 = new Vector(4);
         
         startVectorX0.set(0, new BigDecimal("0"));
@@ -70,11 +85,6 @@ public class JacobiIteration implements Task
         startVectorX0.set(3, new BigDecimal("0"));
         
         
-        
-        MathLib.setPrecision( 6 ); 
-        MathLib.enableRound( true );
-        MathLib.setRoundingMode( MathLib.NORMAL ); 
-        MathLib.setNorm( MathLib.ZEILENSUMMENNORM );
         BigDecimal normM = matrixInput.norm();
         
         formula.addText("M = ").addMatrix(matrixInput).addText(", ").addNewLine(1);
